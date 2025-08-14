befd86f6e2ed6e17e29c5509f3052f1e8adb6e37
Alina <alina.redchin@gmail.com>
2025-08-14 12:55:29 +0200

    fix tests

--- tests/test_products.py
+++ tests/test_products.py
@@ -7,39 +7,34 @@ class TestProducts:
         products_page = Products(driver)
         products_page.open_the_link()
         products_page.login("standard_user", "secret_sauce")
-        products_page.accept_alert()
         assert products_page.is_menu_hidden()
 
     def test_side_menu_items(self, driver):
         products_page = Products(driver)
         products_page.open_the_link()
         products_page.login("standard_user", "secret_sauce")
-        products_page.accept_alert()
-        products_page.click(products_page.OpenMenuButton_locator)
-        products_page.is_menu_expanded()
+        products_page.expand_menu()
         expected_menu_items = [
             "All Items", "About", "Logout", "Reset App State"]
         actual_menu_items = products_page.get_menu_items()
-        assert expected_menu_items == actual_menu_items
+        assert actual_menu_items == expected_menu_items
 
-    def test_redirection_of_About_link(self, driver):
+    def test_redirection_of_about_link(self, driver):
         products_page = Products(driver)
         products_page.open_the_link()
         products_page.login("standard_user", "secret_sauce")
-        products_page.accept_alert()
-        products_page.click(products_page.OpenMenuButton_locator)
+        products_page.expand_menu()
         products_page.is_displayed(products_page.About_button_locator)
         products_page.click(products_page.About_button_locator)
         expected_url = "https://saucelabs.com/"
         current_url = products_page.get_current_url()
         assert expected_url == current_url
 
-    def test_redirection_of_Logout_button(self, driver):
+    def test_redirection_of_logout_button(self, driver):
         products_page = Products(driver)
         products_page.open_the_link()
         products_page.login("standard_user", "secret_sauce")
-        products_page.accept_alert()
-        products_page.click(products_page.OpenMenuButton_locator)
+        products_page.expand_menu()
         products_page.is_displayed(products_page.Logout_button_locator)
         products_page.click(products_page.Logout_button_locator)
         expected_url = "https://www.saucedemo.com/"
@@ -50,7 +45,6 @@ class TestProducts:
         products_page = Products(driver)
         products_page.open_the_link()
         products_page.login("standard_user", "secret_sauce")
-        products_page.accept_alert()
         products_page.expand_filters()
         selected_filter = products_page.find(
             products_page.Selected_filter_option)
@@ -60,7 +54,6 @@ class TestProducts:
         products_page = Products(driver)
         products_page.open_the_link()
         products_page.login("standard_user", "secret_sauce")
-        products_page.accept_alert()
         products_page.expand_filters()
         products_page.get_filter_options()
         expected_filter_options = [
@@ -73,7 +66,6 @@ class TestProducts:
         products_page = Products(driver)
         products_page.open_the_link()
         products_page.login("standard_user", "secret_sauce")
-        products_page.accept_alert()
         products_page.expand_filters()
         products_page.click(products_page.AZ_filter_option_locator)
         expected_product_names = products_page.get_product_names()
@@ -85,12 +77,10 @@ class TestProducts:
         products_page = Products(driver)
         products_page.open_the_link()
         products_page.login("standard_user", "secret_sauce")
-        products_page.accept_alert()
         products_page.expand_filters()
         products_page.click(products_page.ZA_filter_option_locator)
         expected_product_names = products_page.get_product_names()
         actual_product_names = products_page.get_product_names()
-        print(actual_product_names, expected_product_names)
         assert sorted(
             expected_product_names, reverse=True
         ) == actual_product_names, (
@@ -100,12 +90,10 @@ class TestProducts:
         products_page = Products(driver)
         products_page.open_the_link()
         products_page.login("standard_user", "secret_sauce")
-        products_page.accept_alert()
         products_page.expand_filters()
         products_page.click(products_page.HL_filter_option_locator)
         expected_product_price_order = products_page.get_product_price()
         actual_product_price_order = products_page.get_product_price()
-        print(expected_product_price_order, actual_product_price_order)
         assert sorted(
             expected_product_price_order, reverse=True
         ) == actual_product_price_order, (
@@ -115,12 +103,10 @@ class TestProducts:
         products_page = Products(driver)
         products_page.open_the_link()
         products_page.login("standard_user", "secret_sauce")
-        products_page.accept_alert()
         products_page.expand_filters()
         products_page.click(products_page.LH_filter_option_locator)
         expected_product_price_order = products_page.get_product_price()
         actual_product_price_order = products_page.get_product_price()
-        print(expected_product_price_order, actual_product_price_order)
         assert sorted(
             expected_product_price_order
         ) == actual_product_price_order, (
