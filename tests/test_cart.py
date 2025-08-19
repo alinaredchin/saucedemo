1b02d5ba1edd9bfd735ef40ca0fddda4db66c483
Alina <alina.redchin@gmail.com>
2025-08-19 14:32:40 +0200

    add test_continue_shopping_btn_redirects_to_products_list

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -10,3 +10,14 @@ class TestCartPage:
         cart_page.add_to_cart()
         cart_page.click(cart_page.Cart_badge_locator)
         assert cart_page.is_displayed(cart_page.Cart_item_locator)
+
+    def test_continue_shopping_btn_redirects_to_products_list(self, driver):
+        cart_page = Cart(driver)
+        cart_page.open_the_link()
+        cart_page.login("standard_user", "secret_sauce")
+        cart_page.add_to_cart()
+        cart_page.click(cart_page.Cart_badge_locator)
+        cart_page.is_displayed(cart_page.Cart_item_locator)
+        cart_page.click(cart_page.Continue_shopping_button_locator)
+        current_url = cart_page.get_current_url()
+        assert current_url == "https://www.saucedemo.com/inventory.htm"
