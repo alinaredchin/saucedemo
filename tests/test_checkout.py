5df25e55c3a027b8aaf18571be0a34276f0743b7
Alina <alina.redchin@gmail.com>
2025-08-22 22:11:41 +0200

    add test_remove_btn_changes_to_add_to_cart_in_the_checkout_page

--- tests/test_checkout.py
+++ tests/test_checkout.py
@@ -127,4 +127,17 @@ class TestCheckoutPage:
         checkout_page.open_product_details()
         checkout_page.click_back_to_products()
         assert checkout_page.current_url == "https://www.saucedemo.com/inventory.html"
-        
+
+    def test_remove_btn_changes_to_add_to_cart_in_the_checkout_page(self, driver):
+        checkout_page = Checkout(driver)
+        checkout_page.open_the_link()
+        checkout_page.login("standard_user", "secret_sauce")
+        checkout_page.add_to_cart()
+        checkout_page.go_to_cart()
+        checkout_page.go_to_checkout()
+
+        checkout_page.fill_in_the_form("Test", "Test", "12345")
+        checkout_page.click_continue()
+        checkout_page.open_product_details()
+        checkout_page.remove_item()
+        assert checkout_page.verify_element_not_visible(checkout_page.Remove_button_locator)
